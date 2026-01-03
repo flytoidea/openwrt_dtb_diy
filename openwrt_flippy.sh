14573353e8830b97993d1fb38249fb38673d4d06
flytoidea <60851051+flytoidea@users.noreply.github.com>
2026-01-03 17:15:16 +0800

    Update openwrt_flippy.sh

--- openwrt_flippy.sh
+++ openwrt_flippy.sh
@@ -361,6 +361,19 @@ init_packit_repo() {
         fi
         chmod +x ${SELECT_PACKITPATH}/${SCRIPT_DIY}
         echo -e "List of [ ${SELECT_PACKITPATH} ] directory files:\n $(ls -lh ${SELECT_PACKITPATH})"
+         fi
+         echo -e "${STEPS} Auto sync all mk_*.sh scripts to workdir..."
+    for script_file in ${GITHUB_WORKSPACE}/mk_*.sh; do
+        if [[ -f "${script_file}" ]]; then
+            script_name=$(basename "${script_file}")
+            cp -vf "${script_file}" "/opt/${SELECT_PACKITPATH}/${script_name}"
+            chmod +x "/opt/${SELECT_PACKITPATH}/${script_name}"
+            echo -e "${INFO} Synced: [ ${script_name} ]"
+        fi
+    done
+
+
+        
     }
 }
 
